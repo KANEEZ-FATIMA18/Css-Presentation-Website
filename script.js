c35a302109d2cc6e4442e1eac2fe9d742524053a
KANEEZ-FATIMA18 <k.fatima182005@gmail.com>
2025-07-03 14:32:14 +0500

    scrit.js-delete

--- script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const menuIcon = document.getElementById("menu-icon");
-  const navLinks = document.getElementById("nav-links");
-
-  menuIcon.addEventListener("click", function () {
-    navLinks.classList.toggle("show");
-  });
-});
-
